e44e8dab71537857f8ebc6f10dc477458b446b8b
Informatica <informatica@alumno.local>
2024-02-14 13:49:36 -0600

    Se realizo el modelo de estudiante

---
+++ models/estudiantes.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const EstudianteSchema = Schema({
+    nombreE:{
+        type: String,
+        require: [true, "El nombre es obligatorio"]
+    },
+    correoE:{
+        type: String,
+        require: [true, "El correo es obligatorio"],
+        unique: true
+    },
+    paswword:{
+        type: String,
+        require: [true, "La contraseña es Obligatoria"]
+    },
+    habilidad:{
+        type: String,
+        require: [true, "La habilidad debe ser definidad"]
+    },
+    role:{
+        type: String,
+        require: true,
+        enum: ["TEACHER_ROLE", "STUDENT_ROLE"]
+    },
+    curso:{
+        type: String,
+        default: "none"
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model ('Estudiante', EstudianteSchema);
