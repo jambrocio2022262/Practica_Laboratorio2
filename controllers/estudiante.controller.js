a32439295937c14376e031aa31fc22cd8a0fb0e2
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-17 18:48:15 -0600

    Se le cambio el nombre a la base de datos y realizamos cambios en el controlador de estudiante para la funcion que se asigne al curso

--- controllers/estudiante.controller.js
+++ controllers/estudiante.controller.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const Estudiante = require('../models/estudiantes');
 const {response, request, query} = require('express');
+const Curso = require('../models/cursos');
 
 const estudiantesPost = async (req, res) =>{
     const{nombre, correo, password, habilidad, role} = req.body;
@@ -33,7 +34,31 @@ const estudiantesGet = async (req, res = response) =>{
         estudiantes
     ])
 }
+
+const putEstudiante = async (req, res = response) => {
+    const { id } = req.params;
+    const { curso, ...resto } = req.body;
+
+    try {
+        const cursosExistentes = await Curso.find({ _id: { $in: curso } });
+        if (cursosExistentes.length !== curso.length) {
+            return res.status(400).json({ error: '¡No existen esos cursos en la DB!' });
+        }
+
+        const estudiante = await Estudiante.findByIdAndUpdate(id, { ...resto, curso });
+
+        res.status(200).json({
+            msg: '¡Se Actualizo Correctamente el Estudiante',
+            estudiante
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: '¡No se puedo actualizar el Estudiante!' });
+    }
+}
+
 module.exports  = {
     estudiantesPost,
-    estudiantesGet
+    estudiantesGet,
+    putEstudiante
 }
